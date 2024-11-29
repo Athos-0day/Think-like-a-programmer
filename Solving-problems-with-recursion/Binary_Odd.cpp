1872369b59e34f2b380fb7086b1ccec475c1ba17
Athos-0day <athos.0day@protonmail.com>
2024-11-29 11:12:19 +0100

    29-11-24

---
+++ Solving-problems-with-recursion/Binary_Odd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int Iterative_Binary_Odd(int Arr[],int Size) {
+    int total = 0 ;
+    for (int i=0;i<Size;i++) {
+        total += Arr[i] ; //we don't have to verify if Arr[i] is 1 or 0 because 0 doesn't change total
+    }
+    return total/2==0 ;
+}
+
+int Recursive_Total(int Arr[],int Size) {
+        if (Size==0) {
+            return 0 ;
+        }
+        else {
+            return Arr[Size-1]+Recursive_Total(Arr,Size-1) ;
+        }
+    }
+
+int Recursive_Binary_Odd(int Arr[],int Size) {
+    return Recursive_Total(Arr,Size)/2==0 ;
+}
+
+//Same thing with the linked list
+
---
+++ Solving-problems-with-recursion/Sum_of_Positive.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+//Sum the positive number of an Array with an iterative solution and a recursive one 
+int Iterative_Positive_Sum(int Arr[],int Size) {
+    int sum = 0 ;
+    for (int i=0; i<Size; i++) {
+        if (Arr[i]>=0) {
+            sum += Arr[i] ;
+        }
+    }
+    return sum ;
+}
+
+int Recursive_Positive_Sum(int Arr[],int Size) {
+    if (Size==0) {
+        return 0 ;
+    }
+    else if (Arr[Size-1]>=0)
+    {
+        return Arr[Size-1] + Recursive_Positive_Sum(Arr,Size-1) ;
+    }
+    else {
+        return Recursive_Positive_Sum(Arr,Size-1) ;
+    }
+}
+
+//Same but with a linked list 
+struct ListNode {
+    int value ;
+    ListNode * Next ;
+} ;
+
+typedef ListNode * LinkedList ;
+
+int Iterative_Positive_Sum_Lca(LinkedList List) {
+    if (List==NULL) {
+        return 0 ;
+    } 
+    else if (List->value>=0) {
+        return List->value + Iterative_Positive_Sum_Lca(List->Next) ;
+    }
+    else {
+        return Iterative_Positive_Sum_Lca(List->Next) ;
+    }
+
+} 
+
+
+int main() {
+    int Arr[5] ;
+    for (int i = 0; i<5; i++) {
+        Arr[i] = i ;
+        
+    }
+    Arr[2] = -2 ;
+    std::cout << "Iterative : " << Iterative_Positive_Sum(Arr,5) <<" Recursive : " << Recursive_Positive_Sum(Arr,5) << std::endl ;
+}
